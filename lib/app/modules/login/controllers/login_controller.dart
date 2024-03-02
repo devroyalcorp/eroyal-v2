491a5af69ef8525750195be445fc08a295d9ece9
Alghany Kennedy Adam <alghany.adam@royalcorp.id>
2024-03-02 08:15:33 +0700

    feat: routes to home screen

--- lib/app/modules/login/controllers/login_controller.dart
+++ lib/app/modules/login/controllers/login_controller.dart
@@ -1,3 +1,4 @@
+import 'package:eroyal/app/routes/app_pages.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -5,13 +6,10 @@ class LoginController extends GetxController {
   TextEditingController emailOrPhoneNumber = TextEditingController();
   TextEditingController password = TextEditingController();
   final loginKey = GlobalKey<FormState>();
-  final loadingBtn = false.obs;
 
   void login() {
     if (loginKey.currentState!.validate()) {
-      loadingBtn.value = true;
-    } else {
-      "Cant login";
+      Get.toNamed(Routes.HOME);
     }
   }
 }
